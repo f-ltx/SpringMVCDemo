d43daae09d781cd4e7daf4e4376a857628ea353a
f-ltx <li@123456>
2019-01-02 15:17:35 +0800

    test

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,12 @@
+public class TestDemo {
+    public static void main(String[] args) {
+        int x=1;
+        int y=1;
+        if(x>0){
+            System.out.println("x>0");
+        }
+        if(y>0){
+            System.out.println("y>0");
+        }
+    }
+}
